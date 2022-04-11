c263a6766c2868290ca8d2d0d8c4940ebd8cc7fd
dvho <email6@gmail.com>
2022-04-10 23:52:55 -0400

    Restructure directory and add test case to scrabble.test.js

--- scrabble.test.js
+++ scrabble.test.js
@@ -1,11 +1,16 @@
-const sc = require('./scrabble.js');
+const sc = require('./scrabble.js')
 
-const undefinedInputs = sc(undefined);
-if (undefinedInputs !== `Your input is undefined.`) {
-    throw new Error('sc not recognizing undefined inputs');
+const undefinedInputs = sc(undefined)
+if (undefinedInputs !== 'Your input is undefined.') {
+    throw new Error('sc not recognizing undefined inputs')
 }
 
-const nonStringInputs = sc(76589);
-if (nonStringInputs !== `Your input is not a string.`) {
-    throw new Error('sc not recognizing non string inputs');
+const nonStringInputs = sc(76589)
+if (nonStringInputs !== 'Your input is not a string.') {
+    throw new Error('sc not recognizing non string inputs')
+}
+
+const nonAlphabeticCharacters = sc('p4p%oi&ajsdb')
+if (nonAlphabeticCharacters !== 'Your input contains non alphabetic characters.') {
+    throw new Error('sc not recognizing non alphabetic characters')
 }
